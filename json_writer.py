f166563eaa61e08837620d06f3c7edc43bfc3975
Apteno(dytes) <88605874+Apteno-dytes@users.noreply.github.com>
2023-06-17 14:13:29 +0900

    Add files via upload

--- json_writer.py
+++ json_writer.py
@@ -3,6 +3,7 @@ import glob
 import os
 import zipfile
 
+
 class MetaData:
     def __init__(self):
         self.metaVersion = 2
@@ -13,6 +14,7 @@ class MetaData:
         emoji.fileName = fileName
         emoji.emoji.name = os.path.splitext(os.path.basename(fileName))[0]
         self.emojis.append(emoji)
+
 
 class Emoji:
     def __init__(self):
@@ -30,13 +32,10 @@ class Emoji:
             self.isSensitive = False
             self.roleIdsThatCanBeUsedThisEmojiAsReaction = []
 
+
 class MyEncoder(json.JSONEncoder):
     def default(self, o):
-        if isinstance(o, MetaData):
-            return o.__dict__
-        if isinstance(o, Emoji):
-            return o.__dict__
-        if isinstance(o, Emoji.EmojiSpecify):
+        if isinstance(o, MetaData) or isinstance(o, Emoji) or isinstance(o, Emoji.EmojiSpecify):
             return o.__dict__
         return json.JSONEncoder.default(self, o)
 
@@ -46,7 +45,7 @@ if __name__ == '__main__':
     for name in glob.glob('./emojis/*.png'):
         data.addEmoji(os.path.basename(name))
     with open('./emojis/meta.json', 'w') as f:
-        json.dump(data, f, cls = MyEncoder)
+        json.dump(data, f, cls=MyEncoder)
     compFile = zipfile.ZipFile('./emojis.zip', 'w', zipfile.ZIP_DEFLATED)
     for name in glob.glob('./emojis/*'):
         compFile.write(name, os.path.basename(name))
